32581b145ba2ceb707bbee657a124e7ba23ac9b4
Shahed Khan <mdshahedulalamk@gmail.com>
2025-11-11 00:15:07 +0600

    add Why Go Green

---
+++ src/components/WhyGoGreen.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const WhyGoGreen = () => {
+  return (
+    <div className="bg-gray-100 py-15">
+      <h2 className="text-center text-4xl pb-5 poppins font-bold text-[#7a9352] hover:text-[#628727]">
+        Why Go Green?
+      </h2>
+      <div className="roboto font-bold w-11/12 text-center mx-auto">
+        <p className="my-2">
+          Living sustainably isn’t just about saving the planet — it’s about
+          improving our quality of life.
+        </p>
+
+        <p className="my-2">
+          When we go green, we reduce waste, cut down on pollution, and conserve
+          natural resources for future generations.
+        </p>
+
+        <p className="my-2">
+          A greener lifestyle means cleaner air, healthier food, and stronger
+          local communities.
+        </p>
+
+        <p className="my-2">
+          Small actions — like recycling, saving energy, or choosing
+          eco-friendly products — add up to big positive changes when done
+          together.
+        </p>
+
+        <p className="my-2">
+          Going green helps us build a balanced world where people and nature
+          thrive side by side.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default WhyGoGreen;
