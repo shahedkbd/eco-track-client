8cb8b76537a09e3d755d00b726587065a2ff03e7
Shahed Khan <mdshahedulalamk@gmail.com>
2025-11-11 00:07:50 +0600

    add Layout & Pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import WhyGoGreen from '../components/WhyGoGreen';
+import HowItWorks from '../components/HowItWorks';
+
+const Home = () => {
+    return (
+        <div>
+            <WhyGoGreen></WhyGoGreen>
+            <HowItWorks></HowItWorks>
+        </div>
+    );
+};
+
+export default Home;
